e12f4a44ca3b2d3e4f84b28a29d48709847d132b
Josh_Code <rollingmoney2015@gmail.com>
2025-08-22 09:02:42 +0100

    first commit

---
+++ src/main/java/com/cgdms/CGDMS/CgdmsApplication.java
@@ -0,0 +1,13 @@
+package com.cgdms.CGDMS;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CgdmsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CgdmsApplication.class, args);
+	}
+
+}
