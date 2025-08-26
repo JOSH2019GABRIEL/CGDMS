3dc01e9ba5a8ecb98e05735850bb309a7d309ae3
Josh <rollingmoney2015@gmail.com>
2025-08-26 14:19:20 +0100

    improve login

--- src/main/java/com/cgdms/CGDMS/auth/AuthenticationController.java
+++ src/main/java/com/cgdms/CGDMS/auth/AuthenticationController.java
@@ -3,11 +3,9 @@ package com.cgdms.CGDMS.auth;
 
 import com.cgdms.CGDMS.user.User;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.mail.MessagingException;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -20,12 +18,12 @@ public class AuthenticationController {
     @Autowired
     private AuthenticationService service;
 
-//    @PreAuthorize("hasRole('ADMIN')")
-    @PostMapping("/register")
-    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
-        service.register(request);
-        return ResponseEntity.accepted().build();
-    }
+
+//    @PostMapping("/register")
+//    public ResponseEntity<?> register(@RequestBody @Valid RegistrationRequest request) throws MessagingException {
+//        service.register(request);
+//        return ResponseEntity.accepted().build();
+//    }
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthenticationResponse> authenticate (@RequestBody @Valid AuthenticationRequest request) {
@@ -33,10 +31,10 @@ public class AuthenticationController {
 
     }
 
-    @GetMapping("/activate-account")
-    public void confirm (@RequestParam String token) throws MessagingException {
-        service.activateAccount(token);
-    }
+//    @GetMapping("/activate-account")
+//    public void confirm (@RequestParam String token) throws MessagingException {
+//        service.activateAccount(token);
+//    }
 
     @GetMapping("/users")
     public ResponseEntity<List<User>> getUsers (){
