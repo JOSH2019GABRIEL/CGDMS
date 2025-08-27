e34f77d3c88c97a2d1d65372e405281768ba7531
Josh <rollingmoney2015@gmail.com>
2025-08-27 18:50:52 +0100

    Added reusable component for api

---
+++ src/main/webapp/src/api.js
@@ -0,0 +1,8 @@
+// export const token = new URLSearchParams(window.location.search).get('jwt');
+// export const url = '/api/v1/';
+// export const wsUrl = '/websocket';
+
+export const wsUrl = 'http://localhost:8383/websocket';
+export const url = 'http://localhost:9191/api/v1/';
+export const token =
+  'eyJhbGciOiJIUzI1NiJ9.eyJyb2xlIjpbIkFETUlOIl0sImZ1bGxOYW1lIjoiRmlyc3ROYW1lIExhc3RuYW1lIiwic3ViIjoiam9zaDIwMTlnYWJyaWVsQGdtYWlsLmNvbSIsImlhdCI6MTc1NjI4ODk5OCwiZXhwIjoxNzU2Mzc1Mzk4fQ.NvIqfa7xHAqCPDRkR0Pe5ntfjxHDnFBA66cSBGYbBGs';
