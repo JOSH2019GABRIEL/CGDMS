ea17b92540ca27777b1da9eb9325a1b164cab377
Josh <rollingmoney2015@gmail.com>
2025-08-27 11:00:36 +0100

    implement findAll Batches not Archived

--- src/main/java/com/cgdms/CGDMS/batch/BatchRepository.java
+++ src/main/java/com/cgdms/CGDMS/batch/BatchRepository.java
@@ -1,8 +1,18 @@
 package com.cgdms.CGDMS.batch;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface BatchRepository extends JpaRepository<Batch, Long> {
+
+    @Query(value = """
+                SELECT batch 
+                FROM Batch batch
+                WHERE batch.archived = 0
+                """)
+    Page<Batch> findAllNotArchived(Pageable pageable);
 }
