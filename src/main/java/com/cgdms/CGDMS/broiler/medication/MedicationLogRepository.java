70b5938e321ba42a5d100dcf94700437dfbbf72c
Josh <rollingmoney2015@gmail.com>
2025-10-02 22:38:32 +0100

    changeTo the right entity

--- src/main/java/com/cgdms/CGDMS/broiler/medication/MedicationLogRepository.java
+++ src/main/java/com/cgdms/CGDMS/broiler/medication/MedicationLogRepository.java
@@ -1,6 +1,5 @@
 package com.cgdms.CGDMS.broiler.medication;
 
-import com.cgdms.CGDMS.broiler.vaccination.VaccinationLog;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
